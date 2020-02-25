40db3860443c6b8ce3f5ff8b5d749582326074b6
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-02-25 01:08:47 -0500

    Updates

---
+++ precache-manifest.2753882eda2443aa480e357271319c8f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e50384951e24fedf86e9aeca3648189",
+    "url": "/starwarstinder/index.html"
+  },
+  {
+    "revision": "33d91101a6be0f5db180",
+    "url": "/starwarstinder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "586ee709be1c1f1b5f9a",
+    "url": "/starwarstinder/static/css/main.f749d008.chunk.css"
+  },
+  {
+    "revision": "33d91101a6be0f5db180",
+    "url": "/starwarstinder/static/js/2.2353d45b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/starwarstinder/static/js/2.2353d45b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586ee709be1c1f1b5f9a",
+    "url": "/starwarstinder/static/js/main.8fb2d4cb.chunk.js"
+  },
+  {
+    "revision": "5f3300780ad9363fac1f",
+    "url": "/starwarstinder/static/js/runtime-main.5fa644b0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/starwarstinder/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "94ed9421debd43f9f4fbb7e5bd2cbd9a",
+    "url": "/starwarstinder/static/media/greenheart.94ed9421.png"
+  }
+]);
